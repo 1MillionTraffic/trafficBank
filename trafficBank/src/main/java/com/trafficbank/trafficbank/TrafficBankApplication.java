d66a0b75aca0433c23c10666866703a408558d45
GyuHyeok Lim <gyuhyeoklim@GyuHyeokui-MacBookAir.local>
2023-01-03 00:31:49 +0900

    init branch

---
+++ trafficBank/src/main/java/com/trafficbank/trafficbank/TrafficBankApplication.java
@@ -0,0 +1,13 @@
+package com.trafficbank.trafficbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficBankApplication.class, args);
+    }
+
+}
---
+++ trafficBank/src/test/java/com/trafficbank/trafficbank/TrafficBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafficbank.trafficbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
